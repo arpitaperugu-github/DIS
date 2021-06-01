730ceb1e32ac754baa6db133b46a2a58f35a932c
Arpita Perugu <arpita@usf.edu>
2021-05-31 20:33:54 -0400

    Adding Comments

--- Question_4/Question_4/Program.cs
+++ Question_4/Question_4/Program.cs
@@ -4,6 +4,8 @@ namespace Question_4
 {
     class Program
     {
+        /*This method is used to return the index where the sum of all the numbers strictly to the left
+        of the index is equal to the sum of all the numbers strictly to the index's right.*/
         public int PivotIndex(int[] lst)
         {
             int result = -1;
@@ -11,6 +13,7 @@ namespace Question_4
             {
                 int suml = 0;
                 int sumr = 0;
+                //If we are at the leftmost index then the sum to the left is zero and the sum to the right is computed
                 if (i == 0)
                 {
                     suml = 0;
@@ -19,6 +22,7 @@ namespace Question_4
                         sumr = sumr + lst[j];
                     }
                 }
+                //If we are at the rightmost index then the sum to the right is zero and the sum to the left is computed
                 else if (i == (lst.Length) - 1)
                 {
                     sumr = 0;
@@ -38,6 +42,7 @@ namespace Question_4
                         sumr = sumr + lst[j];
                     }
                 }
+                //If the sum to the left is same as sum to the right then we will return that index value else return -1
                 if (suml == sumr)
                 {
                     result = i;
@@ -54,15 +59,19 @@ namespace Question_4
         }
         static void Main(string[] args)
         {
+            //Taking the input from the user
             Console.Write("Please enter the list of numbers seperated by a comma: ");
             string strlst = (Console.ReadLine());
+            //Splitting the input to an array of String
             string[] strlist = strlst.Split(',');
             int[] lst = new int[strlist.Length];
+            //Converting the array of String to array of integer
             for (int i = 0; i < strlist.Length; i++)
             {
                 lst[i] = (int.Parse(strlist[i]));
             }
             Program P = new Program();
+            //Calling the method PivotIndex
             int result = P.PivotIndex(lst);
             Console.Write("The pivot index is: ");
             Console.Write(result);
